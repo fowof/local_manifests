61315a442f05b4fb72c8ae5d3ed6c509967c9177
fowof <7818898+fowof@users.noreply.github.com>
2021-12-19 03:34:31 +0900

    update:

--- setuprepo.sh
+++ setuprepo.sh
@@ -1,4 +1,6 @@
 
+set -e
+
 cd "$(dirname "${BASH_SOURCE:-$0}")"
 
 repo init \
@@ -10,6 +12,10 @@ git clone \
   https://github.com/lin18-microG/local_manifests.git \
   .repo/local_manifests
 
+patch \
+  .repo/local_manifests/setup_sony.xml \
+  togari.patch
+
 mv \
   manifest.xml \
   .repo/local_manifests/setup_sony.xml
@@ -17,4 +23,6 @@ mv \
 [ -d .repo/local_manifests/.git ] && rm -rf .repo/local_manifests/.git
 [ -d .git                       ] && rm -rf .git
 [ -f README.md                  ] && rm README.md
+[ -f manifest.xml               ] && rm manifest.xml
+[ -f togari.patch               ] && rm togari.patch
 [ -f "${BASH_SOURCE:-$0}"       ] && rm "${BASH_SOURCE:-$0}"
