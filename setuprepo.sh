078971438eea3b077ff873f54905d8de9fe22fe3
fowof <7818898+fowof@users.noreply.github.com>
2021-12-25 21:21:25 +0900

    change:

--- setuprepo.sh
+++ setuprepo.sh
@@ -9,18 +9,21 @@ repo init \
   -u https://github.com/LineageOS/android.git \
   -b lineage-18.1
 
-[ -d .repo/local_manifests ] \
-  || mkdir .repo/local_manifests
+git clone \
+  -b lineage-18.1 \
+  https://github.com/lin18-microG/local_manifests \
+  .repo/local_manifests
 
-curl -L \
-  https://raw.githubusercontent.com/lin18-microG/local_manifests/lineage-18.1/updates.xml \
-  -o .repo/local_manifests/updates.xml
+# curl -L \
+#   https://raw.githubusercontent.com/lin18-microG/local_manifests/lineage-18.1/updates.xml \
+#   -o .repo/local_manifests/updates.xml
 
 mv \
   manifest.xml \
-  .repo/local_manifests/togari.xml
+  .repo/local_manifests/setup_togari.xml
 
 [ -d .git                       ] && rm -rf .git
+[ -d .repo/local_manifests/.git ] && rm -rf .repo/local_manifests/.git
 [ -f "${BASH_SOURCE:-$0}"       ] && rm "${BASH_SOURCE:-$0}"
 
 old="$(basename "$(pwd)")"
